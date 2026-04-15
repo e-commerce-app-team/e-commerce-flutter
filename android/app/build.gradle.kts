67aea813b4405d4c6a62146916f8399c8aae6a4d
ALAA_ALDOOS <alaaaldoos123@gmail.com>
2026-04-16 00:49:05 +0400

    onboarding ui

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,24 +8,21 @@ plugins {
 
 android {
     namespace = "com.example.e_commerce"
-    compileSdkVersion 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+        // تفعيل الـ Desugaring لمكتبة الإشعارات
+        isCoreLibraryDesugaringEnabled = true
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID
         applicationId = "com.example.e_commerce"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdkVersion 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -33,17 +30,19 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
+
+kotlin {
+    jvmToolchain(21)
+}
 flutter {
     source = "../.."
 }
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-analytics")
-}
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")}
